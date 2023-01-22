8515db2dc968e9e07db2002142cb3653cf4fb00a
Krzysztof Woźnica <krzysztof2408@o2.pl>
2023-01-22 13:42:38 +0100

    Move RegisterRequestValidator to Authentication/Validators

--- src/core/eShop.Application/Services/Authentication/Validators/RegisterRequestValidator.cs
+++ src/core/eShop.Application/Services/Authentication/Validators/RegisterRequestValidator.cs
@@ -2,14 +2,14 @@
 using FluentValidation;
 
 
-namespace eShop.Application.Validators.Authentication
+namespace eShop.Application.Services.Authentication.Validators
 {
     public class RegisterRequestValidator : AbstractValidator<RegisterRequest>
     {
         public RegisterRequestValidator()
         {
             RuleFor(p => p.FirstName).NotEmpty();
-            RuleFor(p => p.LastName).NotEmpty();    
+            RuleFor(p => p.LastName).NotEmpty();
             RuleFor(p => p.Email).NotEmpty();
             RuleFor(p => p.Password).NotEmpty();
         }
