ec3222683bae73f212d56928a6b069685429d963
Krzysztof Woźnica <krzysztof2408@o2.pl>
2023-02-14 07:44:17 +0100

    Remove duplicate ServiceResponse classes

--- src/core/eShop.Contracts/Common/ServiceResponseValidationResult.cs
+++
@@ -1,9 +0,0 @@
-﻿/*namespace eShop.Contracts.Common
-{
-    public class ServiceResponseValidationResult
-    {
-        public string Key { get; set; }
-        public string Message { get; set; }
-        public string ErrorCode { get; set; }
-    }
-}*/
